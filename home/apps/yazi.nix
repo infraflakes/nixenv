558f4a82fc487251d7bfea30fc1d8d300bfb7666
infraflakes <infraflakes@proton.me>
2025-12-25 17:51:16 +0700

    Set yazi to use imv when open images

--- home/apps/yazi.nix
+++ home/apps/yazi.nix
@@ -10,4 +10,11 @@
     p7zip
     unrar
   ];
+  home.file.".config/yazi/yazi.toml".text = ''
+        [opener]
+    open = [
+        { run = 'imv "$@"', desc = "imv", for = "unix", match = "*.{jpg,jpeg,png,gif,webp}" },
+        { run = 'xdg-open "$@"', desc = "Open", for = "linux" },
+    ]
+  '';
 }
