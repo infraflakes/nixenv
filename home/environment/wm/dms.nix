52ebbb25e44b07268bdfbeea8083b8fa721567b8
infraflakes <infraflakes@proton.me>
2026-01-18 17:06:47 +0700

    18/01

--- home/environment/wm/dms.nix
+++ home/environment/wm/dms.nix
@@ -5,13 +5,13 @@
   ...
 }: {
   imports = [
-    inputs.dms.homeModules.dankMaterialShell.default
+    inputs.dms.homeModules.dank-material-shell
   ];
-  programs.dankMaterialShell = {
+  programs.dank-material-shell = {
     enable = true;
     enableSystemMonitoring = true; # System monitoring widgets (dgop)
     enableDynamicTheming = true; # Wallpaper-based theming (matugen)
     enableAudioWavelength = true; # Audio visualizer (cava)
   };
-  services.cliphist.enable = true;
+  #services.cliphist.enable = true;
 }
