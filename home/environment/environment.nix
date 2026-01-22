4a757c9428aa0a40241ab99c25b1363971028b7d
infraflakes <infraflakes@proton.me>
2026-01-22 13:13:23 +0700

    Disable niri

--- home/environment/environment.nix
+++ home/environment/environment.nix
@@ -4,15 +4,15 @@
   ...
 }: {
   imports = [
-    ./wm/niri/niri.nix
-    ./wm/niri/xwayland.nix
-    ./wm/niri/autostart.nix
-    ./wm/dms.nix
+    # ./wm/niri/niri.nix
+    # ./wm/niri/xwayland.nix
+    # ./wm/dms.nix
 
     ./ui/fonts.nix
     ./ui/theme.nix
     ./ui/wallpapers.nix
 
+    ./sys/autostart.nix
     ./sys/ime.nix
     ./sys/modify-desktop-entry.nix
   ];
--- syswide/runtime/gaming.nix
+++ syswide/runtime/gaming.nix
@@ -9,11 +9,12 @@
     #qtscrcpy
     mangohud
   ];
+  # programs.gamescope.enable = true;
 
   #virtualisation.waydroid.enable = true;
 
   # Add following string to launch arg if game fails to recognize nvidia
-  # VK_ICD_FILENAMES=/run/opengl-driver/share/vulkan/icd.d/nvidia_icd.x86_64.json:run/opengl-driver-32/share/vulkan/icd.d/nvidia_icd.i686.json DXVK_FILTER_DEVICE_NAME="NVIDIA" %command%
+  # VK_ICD_FILENAMES=/run/opengl-driver/share/vulkan/icd.d/nvidia_icd.x86_64.json:run/opengl-driver-32/share/vulkan/icd.d/nvidia_icd.i686.json
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
@@ -21,6 +22,9 @@
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
     #gamescopeSession.enable = true;
   };
+  # programs.steam.package = pkgs.steam.override {
+  #   extraArgs = "-system-composer";
+  # };
   nixpkgs.config.allowUnfreePredicate = pkg:
     builtins.elem (lib.getName pkg) [
       "steam"
