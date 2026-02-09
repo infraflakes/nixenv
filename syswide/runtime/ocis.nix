9aa3fb348651d4e10f0d09bad0d62b267eaa06b2
infraflakes <infraflakes@proton.me>
2026-02-09 21:53:55 +0700

    Utilize environment.variables

--- syswide/runtime/ocis.nix
+++ syswide/runtime/ocis.nix
@@ -8,8 +8,8 @@
   environment = {
     systemPackages = with pkgs; [ocis];
     variables = {
-      # Disable the internal proxy if use Cloudflare Tunnel or Nginx
-      PROXY_TLS = "false";
+      OCIS_CONFIG_DIR = "/data/ocis/config";
+      OCIS_BASE_DATA_PATH = "/data/ocis/data";
     };
   };
   systemd.services.ocis = {
@@ -17,12 +17,17 @@
     after = ["network.target" "local-fs.target"];
     wantedBy = ["multi-user.target"];
 
+    environment = {
+      OCIS_URL = "https://placeholder.trycloudflare.com";
+      PROXY_HTTP_ADDR = "192.168.1.20:9200";
+      PROXY_TLS = "false"; # Disable the internal proxy if use Cloudflare Tunnel or Nginx
+      OCIS_INSECURE = true;
+    };
     serviceConfig = {
       Type = "simple";
       User = username;
       Group = "users";
       WorkingDirectory = "/data/ocis";
-      EnvironmentFile = "/data/ocis/runtime.env";
       ExecStart = "${pkgs.ocis}/bin/ocis server";
       Restart = "always";
     };
