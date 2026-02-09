b3150cb51f498b574c05f243217b4f655c66dce9
infraflakes <infraflakes@proton.me>
2026-02-09 22:26:24 +0700

    Fix ocis cannot find proper user env

--- syswide/runtime/ocis.nix
+++ syswide/runtime/ocis.nix
@@ -5,19 +5,15 @@
   username,
   ...
 }: {
-  environment = {
-    systemPackages = with pkgs; [ocis];
-    variables = {
-      OCIS_CONFIG_DIR = "/data/ocis/config";
-      OCIS_BASE_DATA_PATH = "/data/ocis/data";
-    };
-  };
+  environment.systemPackages = with pkgs; [ocis];
   systemd.services.ocis = {
     description = "ownCloud Infinite Scale";
     after = ["network.target" "local-fs.target"];
     wantedBy = ["multi-user.target"];
 
     environment = {
+      OCIS_CONFIG_DIR = "/data/ocis/config";
+      OCIS_BASE_DATA_PATH = "/data/ocis/data";
       PROXY_HTTP_ADDR = "192.168.1.20:9200";
       PROXY_TLS = "false"; # Disable the internal proxy if use Cloudflare Tunnel or Nginx
       OCIS_INSECURE = "true";
