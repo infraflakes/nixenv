fbb83366dd3bcceae7d2805ecfd6bce87ba5b50f
infraflakes <infraflakes@proton.me>
2026-04-28 21:48:19 +0700

    git ssh

--- home/pkg/cli/vcs.nix
+++ home/pkg/cli/vcs.nix
@@ -5,9 +5,6 @@
   programs.diff-so-fancy = {
     enable = true;
     enableGitIntegration = true;
-    settings = {
-      stripLeadingSymbols = false;
-    };
   };
   programs.git = {
     enable = true;
@@ -24,6 +21,38 @@
       };
     };
   };
+
+  # Generate key: ssh-keygen -t ed25519 -C "infraflakes@proton.me"
+  # ssh-add ~/.ssh/id_ed25519
+  # `cat ~/.ssh/id_ed25519.pub` then copy and make new ssh key on github
+  programs.ssh = {
+    enable = true;
+    enableDefaultConfig = false;
+
+    matchBlocks = {
+      "*" = {
+        addKeysToAgent = "yes";
+      };
+
+      "github.com" = {
+        hostname = "github.com";
+        user = "git";
+        identityFile = "~/.ssh/id_ed25519";
+      };
+      "gitlab.com" = {
+        hostname = "gitlab.com";
+        user = "git";
+        identityFile = "~/.ssh/id_ed25519";
+      };
+    };
+  };
+
+  # Ensure the SSH agent starts with session
+  services.ssh-agent.enable = true;
+  home.sessionVariables = {
+    SSH_AUTH_SOCK = "/run/user/1000/ssh-agent";
+  };
+
   programs.jujutsu = {
     enable = false;
     settings = {
