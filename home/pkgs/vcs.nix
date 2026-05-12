2ed51e1400ebe05a02d890b385061d62a4280b81
infraflakes <infraflakes@proton.me>
2026-05-12 16:38:03 +0000

    Simplify ssh config

--- home/pkgs/vcs.nix
+++ home/pkgs/vcs.nix
@@ -24,24 +24,7 @@
 
   # Generate key: ssh-keygen -t ed25519 -C "infraflakes@proton.me"
   # ssh-add ~/.ssh/id_ed25519
-  # `cat ~/.ssh/id_ed25519.pub` then copy and make new ssh key on github
-  programs.ssh = {
-    enable = true;
-    enableDefaultConfig = false;
-
-    matchBlocks = {
-      "*" = {
-        addKeysToAgent = "yes";
-      };
-      "nix-server" = {
-        hostname = "nix-server";
-        user = "git";
-        identityFile = "~/.ssh/id_ed25519";
-      };
-    };
-  };
-
-  # Ensure the SSH agent starts with session
+  # `cat ~/.ssh/id_ed25519.pub` then copy and make new ssh key
   services.ssh-agent.enable = true;
   home.sessionVariables = {
     SSH_AUTH_SOCK = "/run/user/1000/ssh-agent";
