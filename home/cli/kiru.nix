3fd862757be0447dd34e6e29f466498d9ed7bddf
infraflakes <infraflakes@proton.me>
2026-07-01 11:42:27 +0000

    01/07

--- home/cli/kiru.nix
+++ home/cli/kiru.nix
@@ -1,30 +1,4 @@
 { ... }:
 {
-  home.file.".config/kiru/main.kiru".text = ''
-    var shell workdir = `echo $HOME/test`;
-    sanctuary = $workdir;
-
-    pr kiru {
-      url = `git@github.com:infraflakes/kiru.git`;
-      dir = `kiru`;
-      sync = `clone`;
-      branch = `dev`;
-      include = `.kiru/main.kiru`;
-    }
-
-    pr portfolio {
-      url = `git@github.com:infraflakes/infraflakes.github.io.git`;
-      dir = `portfolio`;
-      sync = `clone`;
-      include = `.kiru/main.kiru`;
-    }
-
-    pr srwc {
-      url = `https://github.com/infraflakes/srwc.git`;
-      dir = `srwc`;
-      sync = `ignore`;
-      branch = `architectural-cleanups`;
-      include = `.kiru/main.kiru`;
-    }
-  '';
+  xdg.configFile."kiru".source = ./config/kiru;
 }
