e89ea10dee258561e828b48733b0adb9f883a175
infraflakes <infraflakes@proton.me>
2026-07-29 00:46:42 +0700

    Fix failing to render braille characters

--- nixos/environment/fonts.nix
+++ nixos/environment/fonts.nix
@@ -1,6 +1,20 @@
 { pkgs, ... }: {
   fonts = {
-    fontconfig.enable = true;
+    fontconfig = {
+      enable = true;
+      # Force fontconfig to override Braille Unicode range with solid dots
+      localConf = ''
+        <fontconfig>
+          <match target="pattern">
+            <test name="family"><string>monospace</string></test>
+            <edit name="family" mode="prepend" binding="strong">
+              <string>Noto Sans Symbols 2</string>
+            </edit>
+          </match>
+        </fontconfig>
+      '';
+    };
+
     packages = with pkgs; [
       noto-fonts
       noto-fonts-cjk-sans
--- nixos/environment/profiles.nix
+++ nixos/environment/profiles.nix
@@ -13,28 +13,23 @@
     enable = true;
     extraPortals = with pkgs; [
       xdg-desktop-portal-gtk
+      # xdg-desktop-portal-gnome
     ];
     config = {
       common = {
-        default = [ "gtk" ];
-        "org.freedesktop.impl.portal.OpenURI" = [ "gtk" ];
-      };
-      srwc = {
         default = [
-          "gnome"
           "gtk"
         ];
-        "org.freedesktop.impl.portal.ScreenCast" = [ "gnome" ];
-        "org.freedesktop.impl.portal.Screenshot" = [ "gnome" ];
+        "org.freedesktop.impl.portal.FileChooser" = [ "gtk" ];
       };
-      niri = {
-        default = [
-          "gnome"
-          "gtk"
-        ];
-        "org.freedesktop.impl.portal.ScreenCast" = [ "gnome" ];
-        "org.freedesktop.impl.portal.Screenshot" = [ "gnome" ];
-      };
+      # niri = {
+      #   default = [
+      #     "gnome"
+      #     "gtk"
+      #   ];
+      #   "org.freedesktop.impl.portal.ScreenCast" = [ "gnome" ];
+      #   "org.freedesktop.impl.portal.Screenshot" = [ "gnome" ];
+      # };
     };
   };
 }
