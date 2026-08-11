025c5ea48eac0568a2d9fa458ece6b7dfc8b4464
infraflakes <infraflakes@proton.me>
2026-08-11 18:59:40 +0700

    Remove ocis

--- server/core/fs.nix
+++ server/core/fs.nix
@@ -19,18 +19,6 @@
     interval = "weekly";
   };
 
-  # sudo zfs create -o mountpoint=legacy iris_pool/ocis
-  # sudo zfs set com.sun:auto-snapshot=true iris_pool/ocis
-  # sudo chown -R $USER:users /data/ocis
-  # OCIS_CONFIG_DIR=/data/ocis/config ocis init
-  # OCIS_CONFIG_DIR=/data/ocis/config ocis idm resetpassword
-  fileSystems."/data/ocis" = {
-    device = "iris_pool/ocis";
-    fsType = "zfs";
-    options = [ "nofail" ];
-    depends = [ "/data" ];
-  };
-
   #doas zfs create -o mountpoint=legacy iris_pool/immich
   #doas zfs set com.sun:auto-snapshot=true iris_pool/immich
   fileSystems."/data/immich" = {
