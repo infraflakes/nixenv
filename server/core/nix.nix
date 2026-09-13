93f402a07eddbb06c836e859d4b42ed1c69956fa
infraflakes <infraflakes@proton.me>
2026-09-13 14:50:29 +0700

    Lix server

--- server/core/nix.nix
+++ server/core/nix.nix
@@ -1,12 +1,15 @@
-{ ... }: {
-  nix.settings = {
-    cores = 0;
-    max-jobs = "auto";
-    auto-optimise-store = true;
-    experimental-features = [
-      "nix-command"
-      "flakes"
-    ];
+{ pkgs, ... }: {
+  nix = {
+    package = pkgs.lix;
+    settings = {
+      cores = 0;
+      max-jobs = "auto";
+      auto-optimise-store = true;
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+    };
   };
   system.stateVersion = "26.11";
   nixpkgs.config.allowUnfree = true;
